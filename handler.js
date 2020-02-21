e1ec1c066f54b2d6f7f568095315eefd57a969df
Tom W <woodard.thomas@gmail.com>
2020-02-20 20:04:34 -0800

    minor update

--- handler.js
+++ handler.js
@@ -1,10 +1,35 @@
 const serverless = require("serverless-http");
-const hbs = require("hbs");
 const express = require("express");
-const bodyParser = require("body-parser");const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
-app.set("view engine", "hbs");app.get("/", function(req, res) {
+const path = require("path");
+var cookie = require('cookie')
+
+const app = express();
+
+app.set('views',path.join(__dirname,"views"))
+app.set("view engine","hbs")
+
+app.use( (req, res, next) => {
+  res.cookie('sessionid', '1', {httpOnly: true, sameSite: 'none', secure: true});
+  next()
+})
+
+app.get("/", (req, res) => {
   res.status(200).render("index");
-});module.exports.elite= serverless(app);
+});
+
+app.get("/ranks", (req, res) => {
+  
+  res.status(200).render("ranks");
+});
+
+app.get("/engineers", (req, res) => {
+  
+  res.status(200).render("engineers");
+});
+
+app.get("/ships", (req, res) => {
+  
+  res.status(200).render("ships");
+});
+
+module.exports.elite= serverless(app);
