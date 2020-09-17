1de43fd08d03169200926e2c6a70a2f83ff31526
Tom Woodard <woodardt@amazon.com>
2020-09-17 06:46:27 -0700

    minor update

---
+++ static/js/master.js
@@ -0,0 +1,48 @@
+//common Scrips for Always-onward.com
+function include(file) {
+  var script  = document.createElement('script');
+  script.src  = file;
+  script.type = 'text/javascript';
+  script.defer = true;
+  document.getElementsByTagName('head').item(0).appendChild(script);
+}
+include('https://stackpath.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js');
+include('js/moment.js');
+include('js/jquery.flexslider-min.js');
+
+(function(){
+  // Mobile menu
+  $('.mobile-menu-icon').click(function(){
+    $('.tm-nav').toggleClass('show');
+  });
+  // http://stackoverflow.com/questions/2851663/how-do-i-simulate-a-hover-with-a-touch-in-touch-enabled-browsers
+  $('body').bind('touchstart', function() {});
+});
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
