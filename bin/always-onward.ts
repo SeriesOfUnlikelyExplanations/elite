43f2df6e993401b0ac3299d28cc74837db2b744d
Tom Woodard <woodardt@amazon.com>
2021-02-02 06:23:10 -0800

    minor update

--- bin/always-onward.ts
+++ bin/always-onward.ts
@@ -5,6 +5,8 @@ import { AlwaysOnwardStack } from '../lib/always-onward-stack';
 
 const app = new cdk.App();
 new AlwaysOnwardStack(app, 'AlwaysOnwardStack', {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: 'us-east-1'
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'us-east-1'
+  }
 });
