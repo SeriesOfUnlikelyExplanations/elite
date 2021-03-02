172528515380abc7382f98be80b7de65ccbc3a09
Tom Woodard <woodardt@amazon.com>
2021-03-01 17:34:58 -0800

    minor update

--- lib/onwardConfig.ts
+++ lib/onwardConfig.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import fs;
 
 export var websiteDistSourcePath = './static';
 export var certificateArn = 'arn:aws:acm:us-east-1:718523126320:certificate/759a286c-c57f-44b4-a40f-4c864a8ab447';
@@ -10,10 +11,10 @@ export var authDomain = 'auth.always-onward.com'
 export var authName = 'auth-onward'
 
 if (fs.existsSync(".env")) {
-    logger.debug("Using .env file to supply config environment variables");
+    console.log("Using .env file to supply config environment variables");
     dotenv.config({ path: ".env" });
 } else {
-    logger.debug("Failed to load");
+    console.log("Failed to load");
 }
 
 export var LWA_CLIENT_ID = process.env.LWA_CLIENT_ID;
