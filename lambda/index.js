f7eefd9e9eeb623fe87f6a6a52e5272e113dc343
Tom Woodard <woodardt@amazon.com>
2021-06-04 09:24:35 -0700

    minor update

--- lambda/index.js
+++ lambda/index.js
@@ -1,23 +1,13 @@
-exports.handler = async function(event) {
-  console.log("request:", JSON.stringify(event, undefined, 2));
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hello, CDK! You've hit ${event.path}\n`
-  };
-};
+// Require the framework and instantiate it
+const api = require('lambda-api')()
 
+// Define a route
+api.get('/status', async (req,res) => {
+  return { status: 'ok' }
+})
 
-//~ // Require the framework and instantiate it
-//~ const api = require('lambda-api')()
-
-//~ // Define a route
-//~ api.get('/status', async (req,res) => {
-  //~ return { status: 'ok' }
-//~ })
-
-//~ // Declare your Lambda handler
-//~ exports.handler = async (event, context) => {
-  //~ // Run the request
-  //~ return await api.run(event, context)
-//~ }
+// Declare your Lambda handler
+exports.handler = async (event, context) => {
+  // Run the request
+  return await api.run(event, context)
+}
