10770f182422f98317482a41d021affe0fb19fb0
Tom Woodard <woodardt@amazon.com>
2021-06-08 19:41:33 -0700

    minor update

--- lambda/auth.js
+++ lambda/auth.js
@@ -2,6 +2,12 @@
 // routes/v1/products.js
 module.exports = (api, opts) => {
   api.get('/clientId', async (req,res) => {
+    res.cors({
+      origin: '*',
+      methods: 'GET, POST, OPTIONS',
+      headers: 'content-type, authorization',
+      maxAge: 84000000
+    })
     return { clientId: '2st20dfpa6esj5hff65aoi9dua' }
-  )
+  })
 }
