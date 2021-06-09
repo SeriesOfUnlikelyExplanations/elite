cb7606fb8bdc216d90a10a008a87949c6a2e7d57
Tom Woodard <woodardt@amazon.com>
2021-06-09 09:02:14 -0700

    minor update

--- lambda/auth.js
+++ lambda/auth.js
@@ -1,13 +1,15 @@
 
 // routes/v1/products.js
 module.exports = (api, opts) => {
-  api.get('/clientId', async (req,res) => {
+  api.get('/parameters', async (req,res) => {
     res.cors({
       origin: '*',
       methods: 'GET, POST, OPTIONS',
       headers: 'content-type, authorization',
       maxAge: 84000000
     })
-    return { clientId: '2st20dfpa6esj5hff65aoi9dua' }
+    return { clientId: '2st20dfpa6esj5hff65aoi9dua'
+      ,region: 'us-west-2'
+      ,identityPoolId: 'us-west-2:48eaf5af-7101-4d73-a749-229113a4a8e3'}
   })
 }
