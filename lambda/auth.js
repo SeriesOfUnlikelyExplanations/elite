621589314ac8be90cdc608dfca95258d00011735
Tom Woodard <woodardt@amazon.com>
2021-06-09 11:01:53 -0700

    minor update

--- lambda/auth.js
+++ lambda/auth.js
@@ -9,6 +9,7 @@ module.exports = (api, opts) => {
     })
     return { clientId: '2st20dfpa6esj5hff65aoi9dua'
       ,region: 'us-west-2'
-      ,identityPoolId: 'us-west-2:48eaf5af-7101-4d73-a749-229113a4a8e3'}
+      ,identityPoolId: 'us-west-2:48eaf5af-7101-4d73-a749-229113a4a8e3'
+      ,UserPoolId: 'us-west-2_Re0YBqOJp'}
   })
 }
