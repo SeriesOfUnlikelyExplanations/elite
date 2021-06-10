ff19739874c99b324d37da3e5e30c65f28dac5b1
Tom Woodard <woodardt@amazon.com>
2021-06-10 06:54:46 -0700

    minor update

--- lambda/index.js
+++ lambda/index.js
@@ -1,4 +1,4 @@
-// Require the framework and instantiate it
+// Require the framework and instantiate it - https://github.com/jeremydaly/lambda-api
 const api = require('lambda-api')()
 
 // Define a route
@@ -7,7 +7,7 @@ api.get('/api/status', async (req,res) => {
 })
 
 api.register(require('./api'), { prefix: '/api' })
-api.register(require('./local'), { prefix: '/*'})
+api.register(require('./local'))
 
 // Declare your Lambda handler
 exports.handler = async (event, context) => {
